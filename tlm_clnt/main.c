a422a77596c6fce8b21a9d3ad36e178af0fb78c0
Brandon Metz <brandonmetz@gmail.com>
2015-03-03 22:37:06 -0800

    ehh made slight change to client to print out all joint pos/vel

--- tlm_clnt/main.c
+++ tlm_clnt/main.c
@@ -13,7 +13,10 @@
 
 int main(int argc, char **argv) {
 	int i=0;
-	int pos=0, vel=0;	
+	int posAz=0, velAz=0;	
+	int posEl=0, velEl=0;	
+	int posElb=0, velElb=0;	
+	int posWrst=0, velWrst=0;	
    key_t key;
    mtrCmdIf_t *mtrCmdIf;
 
@@ -25,9 +28,15 @@ int main(int argc, char **argv) {
 	mtrCmdIf->state = IDLE;
 	while(1){
 		i++;
-		MtrClntSendCmd(mtrCmdIf, MTR_AZ, GET_MTR_POS, &pos, 100);
-		MtrClntSendCmd(mtrCmdIf, MTR_AZ, GET_MTR_VEL, &vel, 100);
-		fprintf(stdout, "%d %d %d\n",i,pos,vel);
+		MtrClntSendCmd(mtrCmdIf, MTR_AZ, GET_MTR_POS, &posAz, 100);
+		MtrClntSendCmd(mtrCmdIf, MTR_AZ, GET_MTR_VEL, &velAz, 100);
+		MtrClntSendCmd(mtrCmdIf, MTR_EL, GET_MTR_POS, &posEl, 100);
+		MtrClntSendCmd(mtrCmdIf, MTR_EL, GET_MTR_VEL, &velEl, 100);
+		MtrClntSendCmd(mtrCmdIf, MTR_ELB, GET_MTR_POS, &posElb, 100);
+		MtrClntSendCmd(mtrCmdIf, MTR_ELB, GET_MTR_VEL, &velElb, 100);
+		MtrClntSendCmd(mtrCmdIf, MTR_WRST, GET_MTR_POS, &posWrst, 100);
+		MtrClntSendCmd(mtrCmdIf, MTR_WRST, GET_MTR_VEL, &velWrst, 100);
+		fprintf(stdout, "%d %d %d %d %d %d %d %d %d\n",i,posAz,velAz,posEl,velEl,posElb,velElb,posWrst,velWrst);
 		fflush(stdout);
 		usleep(100000);
 	};
