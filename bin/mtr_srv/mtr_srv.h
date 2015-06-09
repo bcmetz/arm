81cd0aba98c4f764050df79b4b5c8102d6d1b376
bcmetz <brandonmetz@gmail.com>
2015-06-08 22:45:50 -0700

    Setting up for switching over to 0mq

--- bin/mtr_srv/mtr_srv.h
+++ bin/mtr_srv/mtr_srv.h
@@ -5,30 +5,44 @@
 #define NUM_MTRS 4
 #define TIMEOUT 100	//100 * 10us = 1ms
 
+typedef struct {
+	double gearRatio[NUM_MTRS];
+	double ctsPerRad[NUM_MTRS];
+} mtrParms_t;
+
 typedef enum {
-	IDLE=1,	//Ready for command
-	CLIENT_REQ=2, //Client will set this first to alert other clients
-	SRV_GRANT=4, //Server grants request by setting client ID = reqID
-	RUN_CMD=8, //Client sets cmd id/data then sets flag to this state
-	BUSY=16,		//Server busy processing command
-	DATA_READY=32 //Signal the client to read the data, client must set back to ready
-} srvState_t;
+	MOTION=0,
+	PARAMETER
+} mtrCmdType_t;
+
+//The units are in radians and will be converted to encoder counts here out
+typedef struct {
+	double pos;
+	double velMax;
+	double velFinal;
+	double accel;
+} mtrMove_t;
 
 typedef struct {
-	mtrID_t 		mtrID;
-	cmdID_t 		cmdID;
-	srvState_t	state;
-	uint32_t		data;
-	uint32_t		reqID;
-	uint32_t		grantID;	
-} mtrCmdIf_t;
+	mtrCmdType cmdType;			 //Is this a move or a parameter
+	uint8_t	enabled[NUM_MTRS]; //If true that joints command/move will be processed
+	mtrMove_t move[NUM_MTRS];   //motion details in radians
+	cmdID_t cmdID[NUM_MTRS];	 //command ID for each joint
+	uint32_t cmdData[NUM_MTRS]; //The data to tx with the command ID
+} mtrCmdRcv_t;
 
+typedef struct {
+	uint32_t reply[NUM_MTRS];
+} mtrCmdRep_t;
+
+
+//Messages to publish to subscribers to display record data
 typedef struct {
 	int32_t	pos[NUM_MTRS];
 	int32_t	cmdPos[NUM_MTRS];
 	int32_t	vel[NUM_MTRS];
 	int32_t	voltage[NUM_MTRS];
 	uint32_t	status[NUM_MTRS];
-} mtrTlmIf_t;
+} mtrTlmPub_t;
 
 #endif
